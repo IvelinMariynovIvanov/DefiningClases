f87f800a88209d437e0bbd873a3b95b773302d6b
IvelinMariyanovIvanov <i.ivanov_1987@abv.bg>
2017-02-19 22:59:14 -0800

    a

---
+++ Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClases
+{
+    class Dog
+    {
+        public string name;
+        public string breed;
+        public const string defaultName = null;
+        public const string defaultBred = null;
+
+        public Dog(): this(defaultName, defaultBred)
+        {
+
+        }
+
+        public Dog (string name, string breed)
+        {
+            this.Name = name;
+            this.Breed = breed;
+        }
+
+        public string Name { get; set; }
+        public string  Breed { get; set; }
+
+        public void Bark()
+        {
+            Console.WriteLine(" A dog {0} wiht breed {1} said bauuu", 
+                this.Name ?? "[unnamed dog]", this.Breed ?? "[unknow dog]");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog firstDog = new Dog();
+            Dog secondDog = new Dog("Sharo", "Melez");
+
+            firstDog.Name = "ivo";
+            firstDog.Breed = "Monster";
+
+            firstDog.Bark();
+            secondDog.Bark();
+            firstDog.Bark();
+
+        }
+    }
+}
